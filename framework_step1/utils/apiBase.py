e61a1f46f0b5a09012921d7a007081cdad1fdb6e
VishalDevkate <v.devkate@gmail.com>
2026-06-07 23:04:29 +0530

    draft framework steps1 changes

---
+++ framework_step1/utils/apiBase.py
@@ -0,0 +1,34 @@
+from playwright.sync_api import Playwright, sync_playwright
+
+orderPayload = {"orders": [{"country": "India", "productOrderedId": "6960ea76c941646b7a8b3dd5"}]}
+loginPayload = {
+    "userEmail": "v.d@gmail.com",
+    "userPassword": "RahulShetty@2026"
+}
+
+class ApiUtils:
+    def getToken(self, request_context):
+        response = request_context.post("/api/ecom/auth/login", headers={"Content-Type": "application/json"}, data=loginPayload)
+        assert response.ok
+        token = response.json()["token"]
+        return token
+
+
+    def createorder(self, playwright: Playwright):
+        api_request_context = playwright.request.new_context(base_url="https://rahulshettyacademy.com/")
+
+        token = self.getToken(api_request_context)
+
+        response = api_request_context.post("/api/ecom/order/create-order",
+                                 headers={"Content-Type": "application/json",
+                                          "Authorization" : token},
+                                 data=orderPayload
+                                 )
+        resp = response.json()
+        print("created Order ID: ", resp["orders"][0])
+        return resp["orders"][0]
+
+if __name__ == "__main__":
+    utils = ApiUtils()
+    with sync_playwright() as playwright:
+        utils.createorder(playwright)
